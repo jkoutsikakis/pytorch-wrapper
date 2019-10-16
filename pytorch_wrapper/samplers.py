224725204a69e772acccf97433c4142dde0ec3ea
John Koutsikakis <jkoutsikakis@gmail.com>
2019-10-16 17:44:27 +0300

    add OrderedBatchWiseRandomSampler, SubsetOrderedBatchWiseRandomSampler, OrderedSequentialSampler, and SubsetOrderedSequentialSampler

--- pytorch_wrapper/samplers.py
+++ pytorch_wrapper/samplers.py
@@ -1,4 +1,15 @@
+import random
+
 from torch.utils.data.sampler import Sampler
+
+
+def _batchify(l, batch_size):
+    for i in range(0, len(l), batch_size):
+        yield l[i:i + batch_size]
+
+
+def _flatten(l):
+    return [item for sublist in l for item in sublist]
 
 
 class SubsetSequentialSampler(Sampler):
@@ -18,3 +29,105 @@ class SubsetSequentialSampler(Sampler):
 
     def __len__(self):
         return len(self._indexes)
+
+
+class OrderedBatchWiseRandomSampler(Sampler):
+    """
+    Semi-randomly samples indexes from a dataset ensuring that the corresponding examples will have similar values.
+    Values are returned by a callable.
+    """
+
+    def __init__(self, data_source, get_order_value_callable, batch_size, seed=1234):
+        """
+        :param data_source: a data source (usually a dataset object).
+        :param get_order_value_callable: a callable that takes as input the example's index and returns the ordering
+            value.
+        :param batch_size: the batch size.
+        :param seed: the initial seed.
+        """
+        super(OrderedBatchWiseRandomSampler, self).__init__(None)
+        self._sorted_indexes = sorted(list(range(len(data_source))), key=lambda x: get_order_value_callable(x))
+        self._batch_size = batch_size
+        self._current_seed = seed
+
+    def __iter__(self):
+        self._current_seed += 1
+        rand_state = random.Random(self._current_seed)
+        indexes = list(_batchify(self._sorted_indexes.copy(), self._batch_size))
+        rand_state.shuffle(indexes)
+        return iter(_flatten(indexes))
+
+    def __len__(self):
+        return len(self._sorted_indexes)
+
+
+class SubsetOrderedBatchWiseRandomSampler(Sampler):
+    """
+    Semi-randomly samples indexes from a list ensuring that the corresponding examples will have similar values. Values
+    are returned by a callable.
+    """
+
+    def __init__(self, indexes, get_order_value_callable, batch_size, seed=1234):
+        """
+        :param indexes: a list of indexes.
+        :param get_order_value_callable: a callable that takes as input the example's index and returns the ordering
+            value.
+        :param batch_size: the batch size.
+        :param seed: the initial seed.
+        """
+        super(SubsetOrderedBatchWiseRandomSampler, self).__init__(None)
+        self._sorted_indexes = sorted(indexes, key=lambda i: get_order_value_callable(i))
+        self._batch_size = batch_size
+        self._current_seed = seed
+
+    def __iter__(self):
+        self._current_seed += 1
+        rand_state = random.Random(self._current_seed)
+        indexes = list(_batchify(self._sorted_indexes.copy(), self._batch_size))
+        rand_state.shuffle(indexes)
+        return iter(_flatten(indexes))
+
+    def __len__(self):
+        return len(self._sorted_indexes)
+
+
+class OrderedSequentialSampler(Sampler):
+    """
+    Samples elements from a dataset ordered by a value returned by a callable for each example.
+    """
+
+    def __init__(self, data_source, get_order_value_callable):
+        """
+        :param data_source: a data source (usually a dataset object).
+        :param get_order_value_callable: a callable that takes as input the example's index and returns the ordering
+            value.
+        """
+        super(OrderedSequentialSampler, self).__init__(None)
+        self._sorted_indexes = sorted(list(range(len(data_source))), key=lambda i: get_order_value_callable(i))
+
+    def __iter__(self):
+        return iter(self._sorted_indexes)
+
+    def __len__(self):
+        return len(self._sorted_indexes)
+
+
+class SubsetOrderedSequentialSampler(Sampler):
+    """
+    Samples elements from a list of indexes ordered by a value returned by a callable for each example.
+    """
+
+    def __init__(self, indexes, get_order_value_callable):
+        """
+        :param indexes: a list of indexes.
+        :param get_order_value_callable: a callable that takes as input the example's index and returns the ordering
+            value.
+        """
+        super(SubsetOrderedSequentialSampler, self).__init__(None)
+        self._sorted_indexes = sorted(indexes, key=lambda i: get_order_value_callable(i))
+
+    def __iter__(self):
+        return iter(self._sorted_indexes)
+
+    def __len__(self):
+        return len(self._sorted_indexes)
--- tests/samplers_tests.py
+++ tests/samplers_tests.py
@@ -6,14 +6,86 @@ from pytorch_wrapper import samplers
 class SubsetSequentialSampler(unittest.TestCase):
 
     def test_len(self):
-        indices = [1, 10, 20]
-        sampler = samplers.SubsetSequentialSampler(indices)
+        indexes = [1, 10, 20]
+        sampler = samplers.SubsetSequentialSampler(indexes)
         self.assertEqual(len(sampler), 3)
 
     def test_iter(self):
-        indices = [1, 10, 20]
+        indexes = [1, 10, 20]
         out = []
-        sampler = samplers.SubsetSequentialSampler(indices)
+        sampler = samplers.SubsetSequentialSampler(indexes)
         for idx in sampler:
             out.append(idx)
-        self.assertListEqual(indices, out)
+        self.assertListEqual(indexes, out)
+
+
+class OrderedBatchWiseRandomSampler(unittest.TestCase):
+
+    def test_len(self):
+        data_source = [1, 2, 1, 2, 1, 2]
+        sampler = samplers.OrderedBatchWiseRandomSampler(data_source, lambda x: data_source[x], 3)
+        self.assertEqual(len(sampler), 6)
+
+    def test_iter(self):
+        data_source = [1, 2, 1, 2, 1, 2]
+        out = []
+        sampler = samplers.OrderedBatchWiseRandomSampler(data_source, lambda x: data_source[x], 3)
+        for idx in sampler:
+            out.append(idx)
+
+        for i in range(1, 3):
+            self.assertEqual(data_source[out[i]], data_source[out[0]])
+            self.assertEqual(data_source[out[i+3]], data_source[out[3]])
+
+
+class SubsetOrderedBatchWiseRandomSampler(unittest.TestCase):
+
+    def test_len(self):
+        data_source = [0, 0, 1, 2, 1, 2, 1, 2]
+        indexes = [2, 3, 4, 5, 6, 7]
+        sampler = samplers.SubsetOrderedBatchWiseRandomSampler(indexes, lambda x: data_source[x], 3)
+        self.assertEqual(len(sampler), 6)
+
+    def test_iter(self):
+        data_source = [0, 0, 1, 2, 1, 2, 1, 2]
+        indexes = [2, 3, 4, 5, 6, 7]
+        out = []
+        sampler = samplers.SubsetOrderedBatchWiseRandomSampler(indexes, lambda x: data_source[x], 3)
+        for idx in sampler:
+            out.append(idx)
+        self.assertTrue(
+            (set(out[:3]) == {2, 4, 6} and set(out[3:]) == {3, 5, 7})
+            or (set(out[3:]) == {2, 4, 6} and set(out[:3]) == {3, 5, 7})
+        )
+
+
+class OrderedSequentialSampler(unittest.TestCase):
+
+    def test_len(self):
+        data_source = [1, 10, 20]
+        sampler = samplers.OrderedSequentialSampler(data_source, lambda x: -x)
+        self.assertEqual(len(sampler), 3)
+
+    def test_iter(self):
+        data_source = [1, 10, 20]
+        out = []
+        sampler = samplers.OrderedSequentialSampler(data_source, lambda x: -x)
+        for idx in sampler:
+            out.append(idx)
+        self.assertListEqual([2, 1, 0], out)
+
+
+class SubsetOrderedSequentialSampler(unittest.TestCase):
+
+    def test_len(self):
+        indexes = [1, 10, 20]
+        sampler = samplers.SubsetOrderedSequentialSampler(indexes, lambda x: -x)
+        self.assertEqual(len(sampler), 3)
+
+    def test_iter(self):
+        indexes = [1, 10, 20]
+        out = []
+        sampler = samplers.SubsetOrderedSequentialSampler(indexes, lambda x: -x)
+        for idx in sampler:
+            out.append(idx)
+        self.assertListEqual([20, 10, 1], out)
